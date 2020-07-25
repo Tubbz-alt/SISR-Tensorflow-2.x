64adaa3c935c2c6587573cfcca7b9a34fab6a42a
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-25 10:10:31 +0530

    discriminator

--- src/models/model.py
+++ src/models/model.py
@@ -1,11 +1,39 @@
 import tensorflow as tf 
-from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU
+from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Dense, Flatten, Input, Lambda
 
 
-def convolution_block(inputs, n_filters, filter_size, strides):
+
+
+def convolution_block(inputs, n_filters, filter_size, strides, bn = True, momentum = 0.8):
     x = Conv2D(n_filters, kernel_size=filter_size, strides= strides)(inputs)
-    x = BatchNormalization()(x)
-    return LeakyReLU(x)
+    if bn:
+        x = BatchNormalization(momentum= momentum)(x)
+    return LeakyReLU(alpha=0.2)(x)
+
+def discriminator():
+
+    x_input = Input(shape = (96,96,3))
+    x = Lambda()
+
+    x = convolution_block(x, 64, 3, 1, False)
+    x = convolution_block(x, 64, 3, 1)
+    x = convolution_block(x, 128, 3, 1)
+    x = convolution_block(x, 128, 3, 2)
+    x = convolution_block(x, 256, 3, 1)
+    x = convolution_block(x, 256, 3, 2)
+    x = convolution_block(x, 512, 3, 1)
+    x = convolution_block(x, 512, 3, 2)
+
+    x = Flatten()(x)
+
+
+    x - Dense(1024)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = Dense(1, activation = "sigmoid")
+
+    return x
+
+
 
 
 
@@ -14,3 +42,6 @@ def convolution_block(inputs, n_filters, filter_size, strides):
 #testing for errors
 print(convolution_block(tf.ones((1,2,2,3)), 64,3,1))
 
+print("1 .TESTED \n \n")
+
+print(discriminator(tf.ones((1,2,2,3))))
