8b09178f2295b17164358225f12fc154c14da187
ayushmankumar7 <ayushmankumar7@gmail.com>
2020-07-25 10:32:28 +0530

    normalization

--- src/models/model.py
+++ src/models/model.py
@@ -1,11 +1,11 @@
 import tensorflow as tf 
 from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Dense, Flatten, Input, Lambda
-
+from utils import norm11
 
 
 
 def convolution_block(inputs, n_filters, filter_size, strides, bn = True, momentum = 0.8):
-    x = Conv2D(n_filters, kernel_size=filter_size, strides= strides)(inputs)
+    x = Conv2D(n_filters, kernel_size=filter_size, strides= strides, padding = "same")(inputs)
     if bn:
         x = BatchNormalization(momentum= momentum)(x)
     return LeakyReLU(alpha=0.2)(x)
@@ -13,7 +13,7 @@ def convolution_block(inputs, n_filters, filter_size, strides, bn = True, moment
 def discriminator():
 
     x_input = Input(shape = (96,96,3))
-    x = Lambda()
+    x = Lambda(norm11)(x_input)
 
     x = convolution_block(x, 64, 3, 1, False)
     x = convolution_block(x, 64, 3, 1)
@@ -44,4 +44,4 @@ print(convolution_block(tf.ones((1,2,2,3)), 64,3,1))
 
 print("1 .TESTED \n \n")
 
-print(discriminator(tf.ones((1,2,2,3))))
+print(discriminator())
